355028860a36cc62dfe088ba91605283456a5770
franciscocav <contato_francisco@outlook.com>
2024-02-13 21:39:02 +0000

    experimentos

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Olá mundo!")
+# Bibliotecas que foram usadas para poder realizar este webscraping.
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+website = "https://www.k3distribuidora.com.br/ecommerce/departamento/47/informatica/"
+site = requests.get(website)
+soup = BeautifulSoup(site, 'html.parser')
+print(site.text)
